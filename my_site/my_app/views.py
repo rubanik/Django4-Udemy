3f95684cd85a8a1ef1ba48e3a43b0b2bddb4f2e5
rubanik <anton.rubanik@gmail.com>
2022-12-22 20:13:12 +0300

    here we go

---
+++ my_site/my_app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def example_view(request):
+    return render(request, 'my_app/example.html')
+
+
+def variable_view(request):
+    return render(request, 'my_app/variable.html')
--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)d0lv255ka!2wqy_v*pbs0ss1i8_afs4_737k_*y@g*cthan+#'
+SECRET_KEY = 'django-insecure-z6hn9u(cw%^#x02+0e5)ovycghdc$&%pofub@^u1wzbvi@y7yo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'my_app.apps.MyAppConfig'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'my_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
